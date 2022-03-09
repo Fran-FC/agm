b7184fd16bf36d37856f2e722252846a8ea820bf
Fran-FC <frafolcm@gmail.com>
2022-03-09 17:05:09 +0100

    slides

--- project1/index.js
+++ project1/index.js
@@ -2,12 +2,13 @@ import * as THREE from './threejs/three.module.js';
 import {STLLoader} from './threejs/STLLoader.js';
 import {OrbitControls} from './threejs/OrbitControls.js';
 
+
 let scene, 
     camera, 
     renderer, 
     cameraControls, 
     object,
-    angle;
+    tweenRotation;
 
 // different slides of the page 
 let rotationObject,
@@ -17,13 +18,14 @@ let rotationObject,
     slide3,
     slide4;
 
-var antes = Date.now();
+let photoSphere;
 
 (function init(){
     // RENDERER
     // document.body.appendChild(renderer.domElement);
     const canvas = document.querySelector("#threejs-canvas");
     renderer = new THREE.WebGLRenderer({canvas});
+    renderer.setPixelRatio(window.devicePixelRatio);
 
     
     // SCENE
@@ -34,58 +36,66 @@ var antes = Date.now();
     camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
-        0.1,
+        0.5,
         1000
     );
 
 
-	// Control de camara
-	cameraControls = new OrbitControls( camera, renderer.domElement );
-	cameraControls.target.set( 0, 20, 0 );
+    // Control de camara
+    cameraControls = new OrbitControls( camera, renderer.domElement );
+    cameraControls.target.set( 0, 20, 0 );
     cameraControls.enablePan=false;
+    scene.add(camera);
     scene.add( new THREE.AxesHelper(3) );
     camera.position.set(0,22,20);
     camera.lookAt(new THREE.Vector3(0,20,0)); 
+
     loadScene();
 })();
 
 function loadScene() {
     // background scene image from: https://downloadhdwallpapers.in/sky-cloud/ 
-    var textBackground = new THREE.TextureLoader().load("/images/sky.jpg");
+    var path = "/images/";
+
+
+    // Esfera
+	var entorno = [ path+"posx.jpg", path+"negx.jpg",
+	                path+"posy.jpg", path+"negy.jpg",
+	                path+"posz.jpg", path+"negz.jpg",];
+    var textBackground = new THREE.CubeTextureLoader().load(entorno);
     scene.background = textBackground;
 
     var material = new THREE.MeshPhongMaterial();
     var geometry = new THREE.BoxGeometry(1,1,1);
 
+    { // auxiliar objects
+        rotationObject = new THREE.Mesh(geometry, material.clone());
+        rotationObject.position.set(0,0,0);
+        scene.add(rotationObject);    
 
-    rotationObject = new THREE.Mesh(geometry, material.clone());
-    rotationObject.position.set(0,0,0);
-    scene.add(rotationObject);    
-
-    { // slides
-        // slide2 parent 
         mainSlide = new THREE.Mesh( geometry, material.clone() );
         mainSlide.material.visible = false;
         mainSlide.position.set(0,20,0);
         scene.add(mainSlide);    
-
+    }
+    { // slides
         slide1 = new THREE.Mesh( geometry, material.clone() );
         // slide1.material.visible = false;
         slide1.position.set(0,20,0);
         rotationObject.add(slide1);
         // slide2 parent add
         slide2 = new THREE.Mesh( geometry, material.clone() );
-        slide2.material.visible = false;
+        // slide2.material.visible = false;
         slide2.position.set(20,0,0);
         rotationObject.add(slide2);
         // slide2 parent add
         slide3 = new THREE.Mesh( geometry, material.clone() );
-        slide3.material.visible = false;
+        // slide3.material.visible = false;
         slide3.position.set(0,-20,0);
         rotationObject.add(slide3);
         // slide2 parent add
         slide4 = new THREE.Mesh( geometry, material.clone() );
-        slide4.material.visible = false;
+        // slide4.material.visible = false;
         slide4.position.set(-20,0,0);
         rotationObject.add(slide4);
     }
@@ -115,12 +125,38 @@ function loadScene() {
         scene.add(lightTop);
         // load sliders
     }
+    {//buttons prev, next
+        document.addEventListener("keydown", (ev)=>{
+                if(tweenRotation && tweenRotation.isPlaying()) {
+                    return;
+                }
+                tweenRotation = new TWEEN.Tween(rotationObject.rotation);
+                switch (ev.code) {
+                    case "ArrowLeft":
+                        tweenRotation
+                            .to({ z: rotationObject.rotation.z - Math.PI/2 }, 700)
+                            .easing(TWEEN.Easing.Quadratic.Out)
+                            .start();
+                        
+                        break;
+                    case "ArrowRight":
+                        tweenRotation
+                            .to({ z: rotationObject.rotation.z + Math.PI/2 }, 700)
+                            .easing(TWEEN.Easing.Quadratic.Out)
+                            .start();
+                        break;
+                    default:
+                        break;
+                }
+
+        });
+    }
    
     { // first load     
         // load 3d model of github contribution graph
-        let loader = new STLLoader();
+        let stl_loader = new STLLoader();
         // extracted from https://skyline.github.com/ 
-        loader.load('/3dmodels/Fran-FC-2021.stl', (model)=>{
+        stl_loader.load('/3dmodels/Fran-FC-2021.stl', (model)=>{
             object = new THREE.Mesh(
                 model,
                 new THREE.MeshStandardMaterial({
@@ -136,7 +172,7 @@ function loadScene() {
             slide1.add(object);
         });
         // extracted from https://cults3d.com/en/3d-model/art/github-octocat
-        loader.load('/3dmodels/octo.stl', (model)=>{
+        stl_loader.load('/3dmodels/octo.stl', (model)=>{
             object = new THREE.Mesh(
                 model,
                 new THREE.MeshStandardMaterial({
@@ -151,9 +187,22 @@ function loadScene() {
             object.rotation.x = -Math.PI/2;
             slide1.add(object);
         })
+
+        const photoMaterial = new THREE.MeshBasicMaterial({
+            map: new THREE.TextureLoader().load('images/me.jpeg'),
+        });
+        photoSphere =  new THREE.Mesh( new THREE.SphereGeometry(10,50,50), photoMaterial);
+        photoSphere.position.set(-20, 25, -10);
+        photoSphere.rotation.y = -1;
+        scene.add(photoSphere);
+        var photoRotation = new TWEEN.Tween(photoSphere.rotation)
+                            .to({ y: photoSphere.rotation.y - Math.PI/2 }, 700)
+                            .easing(TWEEN.Easing.Quadratic.Out)
+                            // .start();
+
+
     }
 
-   
     animate();
 }
 
@@ -169,19 +218,11 @@ function resizeRendererToDisplaySize(){
     return needResize;
 }
 
-// update scene for each frame
-function update(){
-    // object.rotations
-	var ahora = Date.now();
-	// Incremento de 20º por segundo
-	rotationObject.rotation.z += Math.PI/3 * (ahora-antes)/1000;
-	antes = ahora;
 
-    console.log("Camera target: "+cameraControls.target.z);
-}
  // refresh loop
 function animate(){
     requestAnimationFrame(animate);
+    TWEEN.update();
 
     // check if renderer needs resize and change camera if it does
     if (resizeRendererToDisplaySize()) {
@@ -190,6 +231,5 @@ function animate(){
         camera.updateProjectionMatrix();
     }
 
-    update();
     renderer.render(scene, camera);
 }
